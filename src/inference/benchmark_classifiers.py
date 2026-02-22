6f972cd6fec6ce0a57f844504058702412e82bcd
Ryan Barbalho <106884891+RyanBarbalho@users.noreply.github.com>
2026-02-22 16:25:52 -0300

    remove centercrop do preprocessamento

--- src/inference/benchmark_classifiers.py
+++ src/inference/benchmark_classifiers.py
@@ -31,7 +31,7 @@ from elsa_streaming_loader import ELSAStreamingDataset
 
 def get_transform():
     return transforms.Compose([
-        transforms.CenterCrop(224),
+        transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
